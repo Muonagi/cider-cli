17b0bc2bb64f721b19eee26a79b451f48a234c42
Muonagi0 <45314403+KageSora0@users.noreply.github.com>
2026-03-28 19:08:41 +0100

    chore: bump version to 0.1.2

--- Formula/cider.rb
+++ Formula/cider.rb
@@ -1,7 +1,7 @@
 class Cider < Formula
   desc "CLI tool for signing and sideloading iOS apps"
   homepage "https://github.com/Muonagi/cider-cli"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
